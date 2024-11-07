b1b81d01dcea2626c1d214996170cfe1c63719d6
Ksenia Kravchenko <kkravchenko@kse.org.ua>
2024-11-07 22:34:54 +0200

    Implement naive approach

---
+++ script.js
@@ -0,0 +1,29 @@
+﻿const form = document.querySelector('.todo');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const input = document.querySelector('.input-element-js');
+    const li = document.createElement('li');
+
+    li.innerHTML = `
+        <label>
+            <input type="checkbox" />
+            ${input.value}
+        </label>
+    `;
+
+    const ul = document.querySelector('.todo-list');
+    ul.appendChild(li);
+
+    input.value = '';
+});
+
+const liRoot = document.querySelector('.todo-list');
+liRoot.addEventListener('change', function(e) {
+    if (e.target.type === 'checkbox') {
+        e.target.closest('li').classList.toggle('done');
+    }
+});
+
+
